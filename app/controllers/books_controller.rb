d7bf75977c606f6820f9360bf1222c9d2ebb9eb5
y.aikawa727@gmail.com <ec2-user@ip-172-31-8-215.ec2.internal>
2021-04-17 10:59:50 +0000

    [Learning4] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,10 +14,17 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/:id'
+    redirect_to book_path(book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
