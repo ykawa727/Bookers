0be0440205301ab0d74cd4cb94bbfc327b27d024
y.aikawa727@gmail.com <ec2-user@ip-172-31-8-215.ec2.internal>
2021-04-17 07:25:30 +0000

    [Learning3] 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root :to => 'homes#top'
 
   resources :books
+end 
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
